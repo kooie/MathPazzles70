1eaca5de0473fefc4ff40e52819489f65f70e8d6
Kouichiro Ie <kouichirou_ie@crossware.co.jp>
2018-10-06 11:11:04 +0900

    CS版のコミット

---
+++ kooie/Q01/Q01Cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Q01Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Q01Cs.getAnswer().ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
