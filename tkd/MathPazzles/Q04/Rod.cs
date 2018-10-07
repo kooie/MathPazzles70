5a326ca02366475c41486d9eccf77bc36ed4a742
Masayuki Takeda <rexiaxm7@gmail.com>
2018-10-07 15:31:16 +0900

    Resolve #4 -- Added Q04 and test to tkd folder

---
+++ tkd/MathPazzles/Q04/Rod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Q04
+{
+    public class Rod
+    {
+        private readonly int _length;
+        public int Length => _length;
+
+        public Rod(int length)
+        {
+            _length = length;
+        }
+
+        public IEnumerable<Rod> CutInHalf()
+        {
+            var halfLength = _length / 2;
+            return new List<Rod>{ new Rod(halfLength), new Rod(_length - halfLength) };
+        }
+    }
+}
