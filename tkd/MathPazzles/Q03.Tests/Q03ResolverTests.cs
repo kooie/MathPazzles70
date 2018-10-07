040b4badccef65516d6744fc9aa33e6ab21682e0
Masayuki Takeda <rexiaxm7@gmail.com>
2018-10-07 15:26:43 +0900

    Resolve #3 -- Added Q03 and test to tkd folder

---
+++ tkd/MathPazzles/Q03.Tests/Q03ResolverTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Q03.Tests
+{
+    [TestFixture]
+    public class Q03ResolverTests
+    {
+        [Test]
+        public void ResolveTest()
+        {
+            var result = Q03Resolver.Resolve();
+            TestContext.WriteLine(string.Join(", ", result));
+        }
+    }
+}
